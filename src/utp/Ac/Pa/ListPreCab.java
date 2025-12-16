aef92604ad238f0887d3bf0864227251fe2e8aba
Benjamin Concepcion Colman <Fliaconcepcioncolman2023@gmail.com>
2025-12-16 04:14:45 -0500

    Create ListPreCab class with constructor

---
+++ src/utp/Ac/Pa/ListPreCab.java
@@ -0,0 +1,12 @@
+public class ListPreCab {
+
+    private int idList;
+    private String name;
+    private boolean active;
+
+    public ListPreCab(int idList, String name) {
+        this.idList = idList;
+        this.name = name;
+        this.active = true;
+    }
+}
