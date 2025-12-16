59667361d9639fb1135f7066871e204c799e9aac
Benjamin Concepcion Colman <Fliaconcepcioncolman2023@gmail.com>
2025-12-16 00:41:21 -0500

    Refactor Role class to Roles with additional fields

--- src/utp/Ac/Pa/Role.java
+++ src/utp/Ac/Pa/Role.java
@@ -1,27 +1,41 @@
-import java.util.Set;
-import java.util.HashSet;
+public class Roles {
 
-public class Role {
-
-    private int idRole;
+    private int id;
     private String name;
-    private Set<Permiso> permisos;
+    private String description;
+    private boolean estadoR;
 
-    public Role(int idRole, String name) {
-        this.idRole = idRole;
+    // 🔹 Constructor
+    public Roles(int id, String name, boolean estadoR /*, String description */) {
+        this.id = id;
         this.name = name;
-        this.permisos = new HashSet<>();
+        this.estadoR = estadoR;
+        //his.description = description;
     }
 
-    public void addPermiso(Permiso permiso) {
-        permisos.add(permiso);
+    // 🔹 Getters y Setters (opcional pero recomendado)
+    public int getId() {
+        return id;
     }
 
-    public boolean hasPermiso(Permiso permiso) {
-        return permisos.contains(permiso);
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+   /*  public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }*/
 }
+
